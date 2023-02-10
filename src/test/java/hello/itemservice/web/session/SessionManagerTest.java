f5b0778d07055095de034efb29321ca620a67085
krust <krust@krustui-MacBookPro-29.local>
2023-02-10 21:08:11 +0900

    making session

---
+++ src/test/java/hello/itemservice/web/session/SessionManagerTest.java
@@ -0,0 +1,58 @@
+package hello.itemservice.web.session;
+
+import hello.itemservice.domain.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionManagerTest {
+
+    SessionManager sessionManager = new SessionManager();
+
+    @Test
+    void sessionTest() {
+
+        // 세션 생성
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        Member member = new Member();
+        sessionManager.createSession(member, response);
+
+        // 요청에 응답 쿠키 저장
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setCookies(response.getCookies());
+
+        // 세션 조회
+        Object result = sessionManager.getSession(request);
+        assertThat(result).isEqualTo(member);
+
+        // 세션 만료
+        sessionManager.expire(request);
+        Object expired = sessionManager.getSession(request);
+        assertThat(expired).isNull();
+    }
+
+    @Test
+    void createSession() {
+    }
+
+    @Test
+    void getSession() {
+    }
+
+    @Test
+    void expire() {
+    }
+
+    @Test
+    void findCookie() {
+    }
+}
